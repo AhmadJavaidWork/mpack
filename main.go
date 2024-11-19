ba21e748f6ed4ecc6cbc07a23da043452ce65fa6
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2024-11-19 19:47:01 +0500

    load file

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello, world!")
+
+	if len(os.Args) < 2 {
+		fmt.Println("provide a json file path. usage path/to/json")
+		os.Exit(1)
+	}
+
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	buffer := []byte{}
+	for scanner.Scan() {
+		buffer = append(buffer, scanner.Bytes()...)
+	}
 }
